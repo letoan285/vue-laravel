dba4f5faa8a38cc5176c998626bf11f483f2f4d6
GOQUOPC\GoQuo-Pc <toanleth.vn@gmail.com>
2017-06-28 11:08:31 +0700

    Initial commit

---
+++ resources/assets/js/components/Home.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+  <nav class="navbar navbar-default">
+        <ul class="nav nav-pills">
+          <li role="presentation"><a href="#">Home</a></li>
+          <li role="presentation"><a href="#">About</a></li>
+          <li role="presentation"><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+    <header></header>
+    
+  </div>
+    
+</template>
+
+<script>
+    import Header from './Header.vue'
+    export default {
+      components: {
+        header: Header
+      },
+      mounted() {
+          console.log('Component mounted.')
+      }
+    }
+</script>
